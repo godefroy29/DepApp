0e3631b8657e8a92ee334ab84286bd7eed6e7f58
godefroy29 <godefroy.29@gmail.com>
2019-04-07 21:41:20 +0200

    Maj BDD

--- Project/DepApp/DepApp/DataBase/DBDepApp.cs
+++ Project/DepApp/DepApp/DataBase/DBDepApp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace DepApp.DataBase
 {
     class DBDepApp
     {
+        SqlConnection conn = new SqlConnection("Data Source=PC;Initial Catalog=DepApp;Integrated Security=SSPI;");
+        //public void Init()
+        //{
+        //    SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder("Data Source=PC;Initial Catalog=DepApp;User id=sa;Password=Adm1n@29;");
+        //    SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder("Data Source=PC;Initial Catalog=DepApp;Integrated Security=SSPI;");
+        //}
     }
 }
